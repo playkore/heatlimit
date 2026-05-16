9563c030c84e692c7abc63f774ac6b0012c22e90
Alexey Korepanov <alexey.korepanov@clickhouse.com>
2026-05-16 12:52:27 +0200

    Add background tile layer

--- godot/heat-limit/scenes/main.gd
+++ godot/heat-limit/scenes/main.gd
@@ -1,14 +1,23 @@
 @tool
 extends Node2D
 
+@onready var background_layer: TileMapLayer = $BackgroundLayer
 @onready var tile_map: TileMapLayer = $TileMapLayer
 
 func _ready() -> void:
+	if background_layer != null and background_layer.get_used_cells().is_empty():
+		_populate_background()
+
 	if tile_map == null:
 		return
 
 	if tile_map.get_used_cells().is_empty():
 		_populate_demo_layout()
+
+func _populate_background() -> void:
+	for y in range(2):
+		for x in range(3):
+			background_layer.set_cell(Vector2i(x, y), 0, Vector2i(x, y), 0)
 
 func _populate_demo_layout() -> void:
 	for y in range(5):
