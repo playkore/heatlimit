2c211e5cd57240f33f7f0b92bfb53d161d76eae5
Alexey Korepanov <alexey.korepanov@clickhouse.com>
2026-05-16 13:06:16 +0200

    Resize Godot viewport for mobile portrait

--- godot/heat-limit/scenes/main.gd
+++ godot/heat-limit/scenes/main.gd
@@ -1,25 +1,12 @@
 @tool
 extends Node2D
 
+const BASE_VIEWPORT_SIZE := Vector2(320, 640)
+
 @onready var background_layer: TileMapLayer = $BackgroundLayer
+@onready var camera: Camera2D = $Camera2D
 @onready var tile_map: TileMapLayer = $TileMapLayer
 
 func _ready() -> void:
-	if background_layer != null and background_layer.get_used_cells().is_empty():
-		_populate_background()
-
-	if tile_map == null:
-		return
-
-	if tile_map.get_used_cells().is_empty():
-		_populate_demo_layout()
-
-func _populate_background() -> void:
-	for y in range(2):
-		for x in range(3):
-			background_layer.set_cell(Vector2i(x, y), 0, Vector2i(x, y), 0)
-
-func _populate_demo_layout() -> void:
-	for y in range(5):
-		for x in range(2):
-			tile_map.set_cell(Vector2i(x, y), 0, Vector2i(x, y), 0)
+	if camera != null:
+		camera.make_current()
